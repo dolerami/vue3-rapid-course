6efa02c8246c0c53773b367c7c2d72d0cdc9340f
David Mheryan <mheryandavid11@gmail.com>
2023-02-10 14:18:53 +0400

    v-for and crating the view by a loop (arrays, objects)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <div class="post">
-      <div><strong>Title:</strong> A post on JavaScript</div>
-      <div><strong>Title:</strong> JavaScript is a universal programming language</div>
-    </div>
-    <div class="post">
-      <div><strong>Title:</strong> A post on JavaScript</div>
-      <div><strong>Title:</strong> JavaScript is a universal programming language</div>
-    </div>
-    <div class="post">
-      <div><strong>Title:</strong> A post on JavaScript</div>
-      <div><strong>Title:</strong> JavaScript is a universal programming language</div>
+    <div class="post" v-for="post in posts">
+<!--    We have a directive v-for, which is the same as "foreach" in JS, so it's making a loop and checking each element inside-->
+<!--    Usually we're setting the arrays' and objects' names in plural-->
+<!--    So when we write v-for, we are referring to each element by writing it's name in singular-->
+      <div><strong>Title:</strong>{{post.title}}</div>
+      <div><strong>Content:</strong>{{post.body}}</div>
+<!--      So as you see here we wrote just 1 HTML tag per element, but we have 4 sections in our browser-->
+<!--      That's how it's working with arrays and objects-->
     </div>
   </div>
 <!--In the template section we write the HTML of the project-->
@@ -20,20 +17,36 @@
 export default{
   data(){
     return{
-      likes:0,
-      dislikes:0,
+      posts:[
+        {
+          id:1,
+          title:'JavaScript',
+          body:'The content of the post',
+        },
+        // So inside the objects we can refer to the object elements by their names
+        {
+          id:2,
+          title:'JavaScript 2',
+          body:'The content of the post 2',
+        },
+        {
+          id:3,
+          title:'JavaScript 3',
+          body:'The content of the post 3',
+        },
+        {
+          id:4,
+          title:'JavaScript 4',
+          body:'The content of the post 4',
+        },
+      ]
+      // You can create a variable and set it as an array as well and make it's content objects
     }
     // here we set the variables just by writing their names and then setting the values
   },
   // data section is for setting the variables, let's say global variables for that specific component
   methods:{
-    addLike(){
-      this.likes += 1;
-      // usually the variables from data section are being called by this. method
-    },
-    addDislike(){
-      this.dislikes += 1;
-    }
+
   }
   // methods section is where you usually set your functions to operate them later
 }
