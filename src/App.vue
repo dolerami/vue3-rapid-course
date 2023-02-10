bacb0a996feb686ef3be95f581accf829b5da6de
David Mheryan <mheryandavid11@gmail.com>
2023-02-10 19:22:25 +0400

    v-model

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,6 @@ export default{
           title:'JavaScript',
           body:'The content of the post',
         },
-        // So inside the objects we can refer to the object elements by their names
         {
           id:2,
           title:'JavaScript 2',
@@ -51,48 +50,14 @@ export default{
           body:'The content of the post 4',
         },
       ],
-      // You can create a variable and set it as an array as well and make it's content objects
-      title:'',
-      body:'',
     }
-    // here we set the variables just by writing their names and then setting the values
   },
   // data section is for setting the variables, let's say global variables for that specific component
-  methods:{
-    createPost(){
-      const newPost={
-        // Here we're setting a new local variable while clicking the button, which is an object itself
-        // So this object has all the necessary fields as the objects above have
-        id:Date.now(),
-        // As an id we will take the current date with this method
-        title:this.title,
-        body:this.body,
-        // And we're getting the values of our title and body models to set them in the new object after clicking the button
-      }
-      this.posts.push(newPost);
-      // So after setting the variable object we're pushing it as a new object next to the others
-      // But this method won't work now as we have to learn something else for it
-      // So this is a usual thing for the browser as a reaction to the actions of submit
-      // If we need to cancel the by default actions of the browser in the form, we have to call the method prevent default
-      // We could do that by writing an argument "event" in the method
-      // And later adding event.stopPropagation() or/and event.preventDefault()
-      // But we're in Vue 3, so we can do it with modifications, which is easier
-      this.title = '';
-      this.body = '';
-      // This is important to have clean inputs after the object creation
-    },
-    // inputTitle(event){
-    //   // we are sending the event as an argument for this method
-    //   // this is important, because else it will not understand what event we're talking about
-    //   this.title=event.target.value;
-    //   // here we've said that the model title is equal to the input event target's value
-    //   // to understand what is that event target value, we can do console.log(event); and look in the details
-    // },
+  methods: {
+    createPost() {
+    }
   }
-  // methods section is where you usually set your functions to operate them later
 }
-// When you create a component, you have to export it and everything in it as an object, that's why it's being written in {}
-// In the script section we write the model and methods of the project
 </script>
 
 <style>
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -6,45 +6,27 @@
     <!--      So we will be able to add a new object, and it will be visible and not get lost by refreshing the browser-->
     <h4>The creation of a post</h4>
     <input
-        :value="post.title"
-        @input="post.title=$event.target.value"
+        v-model="post.title"
         class="input"
         type="text"
         placeholder="Title"
     >
-    <!--      v-bind directive helps us to bind some data with a particular component-->
-    <!--      So after v-bind: you have to write something, usually an attribute of that tag-->
-    <!--      In this case it's the value, so we've bound the value of the input with the variable "title"-->
-    <!--      Then you write the model(variable) you want to bind the attribute with, here it's "title"-->
-    <!--      After that you have to write also a v-on:(event) or @(event) and set a method for that-->
-    <!--      Here this event is input, so that the method will work when you input or delete something in the input line-->
-    <!--      Then you have to set a method for that, we will comment that method below in the methods' chapter-->
-    <!--      As a result whatever you insert in the input "title" as a value, it will be inserted in the model "title" too-->
-    <!--      But we can make it even simpler, so we dont have to write a separate method and do a v-on with the event-->
-    <!--      So let's keep this as a comment here-->
-    <!--      @input="inputTitle"-->
-    <!--      And let's write it another way - @input="title=$event.target.value"-->
-    <!--      Here we've said that our variable "title" takes the value of the event's target-->
-    <!--      in this case we don't need the method "inputTitle" below, so I'll comment it-->
-    <!--      So v-bind: has an easier writing method as well, which is just :-->
     <input
-        :value="post.body"
-        @input="post.body=$event.target.value"
+        v-model="post.body"
         class="input"
         type="text"
         placeholder="Content"
     >
-<!--    As we've created child components, we need to send the information from child to the parent to be visible in browser-->
-<!--    For that we need to create a new model here - posts, which is an object with variables title and body-->
-<!--    After that we have to change the v-bind and v-on in the inputs, referring to the post first, then to the title and body-->
-<!--    so instead :value="title"/"body" & @input="title/body=$event.target.value" we will write "...post.title"/"post.body"-->
+<!--    So v-model is the short way to write v-bind and v-on, it's doing both functions at the same time-->
+<!--    It's not a thing to write everywhere, but is very useful-->
+<!--    in Vue 3 you can use it many times in a component, while in Vue 2 you can do it only once-->
     <button
         class="btn"
+        @click="createPost"
     >
       Create
     </button>
     <!--      Here we're operating a new method when clicking the button, so the comments are next to the method-->
-    <!--    For now let's cut this from the button - @click="createPost"-->
   </form>
 </template>
 
@@ -57,6 +39,17 @@ export default {
         body:'',
       }
     }
+  },
+  methods:{
+    createPost(){
+      this.post.id = Date.now();
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+      // We have cut the method from the parent and simply put here
+      // Just changed the variable, cause we don't need it
+      // Only added an id to the model
+    },
   }
 }
 </script>
