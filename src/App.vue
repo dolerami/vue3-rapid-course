5b0727eb1ef5d1e969b4b72fcbc1ba1c588eba35
David Mheryan <mheryandavid11@gmail.com>
2023-02-10 13:06:47 +0400

    creating the app component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    It's working!
+    <button>Click me</button>
+    <input type="text">
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
