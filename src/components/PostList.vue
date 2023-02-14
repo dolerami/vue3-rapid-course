9f13d0de2fa63d0ca73efb5acd0a9e57783e8e13
David Mheryan <mheryandavid11@gmail.com>
2023-02-14 18:12:20 +0400

    v-if/v-else and v-show

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,13 @@
 <template>
 <!--  In Vue2 we have to have a root tag here, f.i. div, but in Vue3 it's not mandatory-->
 <!--But when we use loops in Vue3, it's better to have a root tag-->
-  <div>
-    <h3>Users List</h3>
+  <div v-if="posts.length > 0">
+<!--    Here we've set a new condition with v-if directive-->
+<!--    We say that the div is active or visible only when the length of the posts array is more than 0-->
+<!--    In other words, when we have at least 1 visible post in the post list-->
+<!--    v-if can work with v-else, which is usually following the element with v-if-->
+<!--    If the condition in v-if isn't true, then v-else is working-->
+    <h3>Users list</h3>
     <post-item
         v-for="post in posts"
         :post="post"
@@ -14,6 +19,21 @@
     <!--      But as soon as here we're using a loop, we tell him to set a key parameter as well-->
     <!--      The key is the id of each post, meaning that in the loop for each post the key parameter will be its id-->
   </div>
+  <h2 v-else style="color: red">
+<!--    We said that if the list is empty, let this tag be active-->
+<!--    We can also set a chain of conditions with v-else-if, but here we dont need it-->
+<!--    **************** v-show *********************-->
+<!--    It's possible to do the same things with v-show directive-->
+<!--    What we need is to write in the main div v-show="posts.length > 0"-->
+<!--    And in this 'h2' tag we need to write v-show="posts.length === 0"-->
+<!--    The result will be the same, but there is an important difference between these directives-->
+<!--    During v-show the inactive element is not deleted from the DOM tree, it just has a display:none style-->
+<!--    While during v-if/v-else the inactive element doesn't exist in the DOM tree -->
+<!--    This can be problematic in some cases, f.e. when we use an attribute from the non-visible element-->
+<!--    If we use v-if/v-else, in that case this attribute will not be used, so we will have an issue-->
+<!--    We need to be careful and attentive while deciding which method to use for hiding/showing some elements-->
+    Users list is empty
+  </h2>
 </template>
 
 <script>
